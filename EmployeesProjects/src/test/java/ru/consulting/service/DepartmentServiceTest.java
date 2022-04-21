8d429c943ce1b30c26d0e319ad640efc259ae6aa
Romanr1995 <roman.nozdracheff@yandex.ru>
2022-04-22 00:24:21 +0300

    Update DepartmentServiceTest

--- EmployeesProjects/src/test/java/ru/consulting/service/DepartmentServiceTest.java
+++ EmployeesProjects/src/test/java/ru/consulting/service/DepartmentServiceTest.java
@@ -13,12 +13,14 @@ import ru.consulting.repositories.EmployeeRepo;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class DepartmentServiceTest {
 
+    private static final long DEPARTMENT_ID = 5l;
+    private static final String EMP_PHONE = "89997776654";
+    private static final String EMP_EMAIL = "emptest@yandex.ru";
     private DepartmentService departmentService;
     private DepartmentRepo departmentRepo;
     private EmployeeRepo employeeRepo;
@@ -28,6 +30,8 @@ public class DepartmentServiceTest {
         departmentRepo = mock(DepartmentRepo.class);
         employeeRepo = mock(EmployeeRepo.class);
         departmentService = new DepartmentService(departmentRepo, employeeRepo);
+
+        when(departmentRepo.findById(DEPARTMENT_ID)).thenReturn(Optional.of(getDepartment()));
     }
 
     @Test
@@ -44,21 +48,19 @@ public class DepartmentServiceTest {
 
     @Test
     void delete_exist_department() {
-        Long id = 5l;
-        when(departmentRepo.existsById(id)).thenReturn(true);
+        when(departmentRepo.existsById(DEPARTMENT_ID)).thenReturn(true);
 
-        departmentService.removeById(id);
+        departmentService.removeById(DEPARTMENT_ID);
 
-        verify(departmentRepo).deleteById(id);
+        verify(departmentRepo).deleteById(DEPARTMENT_ID);
     }
 
 
     @Test
     void delete_non_exist_department() {
-        Long id = 5l;
-        when(departmentRepo.existsById(id)).thenReturn(false);
+        when(departmentRepo.existsById(DEPARTMENT_ID)).thenReturn(false);
 
-        Executable executable = () -> departmentService.removeById(id);
+        Executable executable = () -> departmentService.removeById(DEPARTMENT_ID);
 
         assertThrows(NoSuchEntityException.class, executable);
     }
@@ -75,25 +77,59 @@ public class DepartmentServiceTest {
 
     @Test
     void update_department_head() {
-        Long depId = 1L;
-        Department department = new Department();
-        department.setId(depId);
-        when(departmentRepo.findById(depId)).thenReturn(Optional.of(department));
-
-        String empPhone = "89997776654";
-        String empEmail = "emptest@yandex.ru";
-        Employee employee = new Employee();
-        employee.setPhone(empPhone).setEmail(empEmail);
-        when(employeeRepo.findByPhoneOrEmailIgnoreCase(empPhone, empEmail)).thenReturn(Optional.of(employee));
-        department.setDepartmentHead(employee);
+        Employee employee = getEmployee(EMP_PHONE, EMP_EMAIL);
+        when(employeeRepo.findByPhoneOrEmailIgnoreCase(EMP_PHONE, EMP_EMAIL)).thenReturn(Optional.of(employee));
         ArgumentCaptor<Department> argCaptor = ArgumentCaptor.forClass(Department.class);
 
-        departmentService.updateDepartmentHead(depId, empPhone, empEmail);
+        departmentService.updateDepartmentHead(DEPARTMENT_ID, EMP_PHONE, EMP_EMAIL);
 
         verify(departmentRepo).save(argCaptor.capture());
-        assertEquals("89997776654", argCaptor.getValue().getDepartmentHead().getPhone());
-        assertEquals("emptest@yandex.ru", argCaptor.getValue().getDepartmentHead().getEmail());
+        assertEquals(EMP_PHONE, argCaptor.getValue().getDepartmentHead().getPhone());
+        assertEquals(EMP_EMAIL, argCaptor.getValue().getDepartmentHead().getEmail());
+    }
+
+    @Test
+    void update_department_head_with_depId_no_exist() {
+        when(departmentRepo.findById(3L)).thenReturn(Optional.ofNullable(null));
+
+        Executable executable = () -> departmentService.updateDepartmentHead(DEPARTMENT_ID, EMP_PHONE, EMP_EMAIL);
+
+        verifyNoInteractions(employeeRepo);
+        assertThrows(NoSuchEntityException.class, executable);
+    }
+
+    @Test
+    void update_department_head_with_empPhone_and_empEmail_is_null() {
+        when(employeeRepo.findByPhone(null)).thenThrow(new IllegalArgumentException());
+
+        Executable executable = () -> departmentService.updateDepartmentHead(DEPARTMENT_ID, null, null);
+
+        assertThrows(IllegalArgumentException.class, executable);
+    }
+
+    @Test
+    void update_department_head_only_empEmail_null() {
+        Employee employee = getEmployee(EMP_PHONE, null);
+        when(employeeRepo.findByPhone(EMP_PHONE)).thenReturn(employee);
+        ArgumentCaptor<Department> argCaptor = ArgumentCaptor.forClass(Department.class);
+
+        departmentService.updateDepartmentHead(DEPARTMENT_ID, EMP_PHONE, null);
+
+        verify(departmentRepo).save(argCaptor.capture());
+        assertEquals(EMP_PHONE, argCaptor.getValue().getDepartmentHead().getPhone());
+        assertNull(argCaptor.getValue().getDepartmentHead().getEmail());
     }
 
 
+    private Department getDepartment() {
+        Department department = new Department();
+        department.setId(DEPARTMENT_ID);
+        return department;
+    }
+
+    private Employee getEmployee(String phone, String email) {
+        Employee employee = new Employee();
+        employee.setPhone(phone).setEmail(email);
+        return employee;
+    }
 }
